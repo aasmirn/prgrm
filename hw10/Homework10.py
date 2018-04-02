8c7d4d61a93f22f223f7c63d5f3628581c4103dc
AlinaSmirnova17 <aasmirnova_14@edu.hse.ru>
2018-04-02 13:26:38 +0300

    hw updated

--- hw10/Homework10.py
+++ hw10/Homework10.py
@@ -13,13 +13,15 @@ def main():
     if os.path.exists(file) == False:
         print('Файл не найден.')
     else:
-        m = re.findall(r'<a href="/wiki/UTC.*?" class="mw-redirect" title="UTC.*?">(.*?)</a>', fread(file))
-        if m == []:
-            print('К сожалению, на странице нет информации о часовом поясе.')
-        if len(m) == 1:
-            print('Часовой пояс вашего города:', m[0])
-        else:
-            print('Часовой пояс вашего города зимой: ' + m[0] + '; летом: ' + m[1])
+        with open('info.txt', 'w', encoding='utf-8') as f:
+            m = re.findall(r'<a href="/wiki/UTC.*?" class="mw-redirect" title="UTC.*?">(.*?)</a>', fread(file))
+            if len(m) == 0:
+                f.write('К сожалению, на странице нет информации о часовом поясе.')
+            if len(m) == 1:
+                f.write('Часовой пояс вашего города: '+m[0])
+            if len(m) > 1:
+                f.write('Часовой пояс вашего города зимой: ' + m[0] + '; летом: ' + m[1])
+        print('Информация записана в файл info.txt')
         
 if __name__ == '__main__':
     main()
